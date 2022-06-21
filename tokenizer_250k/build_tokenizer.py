024f0b97b23b27a2b88f7e2f7509c315fafe675c
bonaventuredossou <femipancrace.dossou@gmail.com>
2022-06-21 15:54:55 -0400

    Fixed tokenizer issues

--- tokenizer_250k/build_tokenizer.py
+++ tokenizer_250k/build_tokenizer.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 import sentencepiece as spm
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 langs = ['amh', 'hau', 'lug', 'luo', 'pcm', 'sna', 'tsn', 'wol', 'ewe', 'bam', 'bbj', 'mos', 'zul', 'lin', 'nya', 'twi',
          'fon', 'ibo', 'kin', 'swa', 'xho', 'yor', 'oro']
 
-dataset = '../dataset/{}_mono.tsv'
+dataset = 'dataset/all_mono/{}_mono.tsv'
 
 
 def save_list(lines, filename):
@@ -20,14 +23,24 @@ def main():
         # shuffle the training set for this active learning round
         current_dataset = pd.read_csv(dataset.format(lang), sep='\t')
         current_dataset = current_dataset.sample(frac=1)
+
+        # x_train, test = train_test_split(current_dataset, test_size=0.2, random_state=1234)
+        # train, valid = train_test_split(x_train, test_size=0.1, random_state=1234)
         all_pds.append(current_dataset)
 
     concats = pd.concat(all_pds)
-    save_list(concats.input.tolist(), '../data/txt/all_train.txt')
-    print('Building SPM')
-    spm.SentencePieceTrainer.Train(input='../data/txt/all_train.txt', model_prefix='sentencepiece.bpe',
-                                   vocab_size=512000,
-                                   character_coverage=0.9995, model_type='bpe')
+    save_list(concats.input.tolist(), 'data/txt/all_train.txt')
+    spm.SentencePieceTrainer.Train(input='data/txt/all_train.txt', model_prefix='sentencepiece.bpe',
+                                   vocab_size=250000, pad_id=1,
+                                   unk_id=3,
+                                   bos_id=0,
+                                   eos_id=2,
+                                   pad_piece='<pad>',
+                                   unk_piece='<unk>',
+                                   bos_piece='<s>',
+                                   eos_piece='</s>',
+                                   character_coverage=0.9995,
+                                   model_type='bpe')
 
 
 if __name__ == '__main__':
